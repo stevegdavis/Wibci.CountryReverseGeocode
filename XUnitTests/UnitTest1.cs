23883636aadc9412f1ef599751dd2f58571bbc67
Carl Scotney <carl@unlockeddata.com>
2025-07-23 16:32:10 +0100

    gitignore updates

---
+++ XUnitTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using UnlockedData.CountryReverseGeocode.Models;
+using UnlockedData.CountryReverseGeocode;
+namespace XUnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+        var countryService = new CountryReverseGeocodeService();
+        var location = new GeoLocation()
+        {
+            Latitude = 51.00,
+            Longitude = 0
+        };
+
+        var country = countryService.FindCountry(location);
+        
+        Console.WriteLine(country.Name);;
+    }
+}
