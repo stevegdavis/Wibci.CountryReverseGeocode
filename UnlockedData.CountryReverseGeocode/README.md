d1e9bb1856d1a1bac863e9baea5963fd5eacb22d
stevegdavis <stevedavis@post.com>
2026-01-09 14:18:57 +0000

    Added some new countries and improved accuracy of some countries

--- UnlockedData.CountryReverseGeocode/README.md
+++ UnlockedData.CountryReverseGeocode/README.md
@@ -1,4 +1,6 @@
-﻿# SDSDevelopment.UnlockedData.CountryReverseGeocode
-
+﻿# SDSDevelopment.Wibci.CountryReverseGeocode
+Reverse geocode a country or american state based on location point (latitude ; longitude) All code is offline - no api calls to map services necessary
 ## Built from 
 https://github.com/InquisitorJax/Wibci.CountryReverseGeocode
+
+Added some new countries and improved accuracy of some countries mostly in Europe.
--- XUnitTests/UnitTest1.cs
+++ XUnitTests/UnitTest1.cs
@@ -11,12 +11,21 @@ public class UnitTest1
         var countryService = new CountryReverseGeocodeService();
         var location = new GeoLocation()
         {
-            Latitude = 51.00,
-            Longitude = 0
+            Latitude = 49.007858265297706,
+           
+
+
+            Longitude = -123.06867315982991
+
+        };
+        var locationNeth = new GeoLocation()
+        {
+            Latitude = 53.368723,//Netherlands
+            Longitude = 5.216398
         };
 
         var country = countryService.FindCountry(location);
         
-        Console.WriteLine(country.Name);;
+        Console.WriteLine(country.Name);
     }
 }
